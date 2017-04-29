254656fb831c18136087fdb9e601c3099ae2923c
seyed <snaba104@uottawa.ca>
2017-04-28 22:29:26 -0400

    Routing

--- Headlines.py
+++ Headlines.py
@@ -4,14 +4,17 @@ import feedparser
 
 
 app=Flask(__name__)
-BBC_FEED="http://feeds.bbci.co.uk/news/rss.xml"
-@app.route("/",methods=["GET"])
-def get_news():
-    feed = feedparser.parse(BBC_FEED)
+SS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'fox': 'http://feeds.foxnews.com/foxnews/latest',
+             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+@app.route("/<publication>",methods=["GET"])
+def get_news(publication):
+    feed = feedparser.parse(SS_FEEDS[publication])
     first_article = feed['entries'][0]
     return """<html>
         <body>
-            <h1> BBC Headlines </h1>
+            <h1>  Headlines </h1>
             <b>{0}</b> <br/>
             <i>{1}</i> <br/>
             <p>{2}</p> <br/>
