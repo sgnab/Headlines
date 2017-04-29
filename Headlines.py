68e2e72b480f1eb8804dc9441a5d0a07115b2e5d
seyed <snaba104@uottawa.ca>
2017-04-29 00:12:35 -0400

    templates

--- Headlines.py
+++ Headlines.py
@@ -11,15 +11,16 @@ SS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 @app.route("/<publication>",methods=["GET"])
 def get_news(publication):
     feed = feedparser.parse(SS_FEEDS[publication])
-    first_article = feed['entries'][0]
-    return """<html>
-        <body>
-            <h1>  Headlines </h1>
-            <b>{0}</b> <br/>
-            <i>{1}</i> <br/>
-            <p>{2}</p> <br/>
-        </body>
-    </html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+    all_article = feed['entries']
+    return render_template('newsfeed.html',all_article=all_article)
+    # return """<html>
+    #     <body>
+    #         <h1>  Headlines </h1>
+    #         <b>{0}</b> <br/>
+    #         <i>{1}</i> <br/>
+    #         <p>{2}</p> <br/>
+    #     </body>
+    # </html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
 
 
 
